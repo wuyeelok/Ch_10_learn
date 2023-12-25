bac29df7dc444ba14d850125c213e6699b374ffe
wuyeelok <wuyeelok@gmail.com>
2023-12-25 17:51:36 +0800

    Learning stream findAny, reduce

--- src/stream/LearnStream.java
+++ src/stream/LearnStream.java
@@ -15,5 +15,20 @@ public class LearnStream {
         var myStream2 = Stream.of("a32", "sddfsfewfe", "dsfsdf");
 
         myStream2.min(Comparator.comparingInt(String::length)).ifPresent(m -> System.out.println("smallest: " + m));
+
+        var sStream = Stream.of("21323a32", "sddfsfewfe", "dsfsdf", "234");
+        sStream.findFirst().ifPresent(s -> System.out.println("Find first: " + s));
+
+        Stream<String> s = Stream.of("MMM", "ggg", "bonon");
+        s.findAny().ifPresent(System.out::println);
+
+        Stream<String> infi = Stream.generate(() -> "HaHa");
+        infi.findAny().ifPresent(System.out::println);
+
+        Stream<Integer> intS = Stream.of(2, 4, 1, 2, 5);
+        int sum = intS.reduce(0, Integer::sum);
+        System.out.println("Sum is: " + sum);
+
+
     }
 }
